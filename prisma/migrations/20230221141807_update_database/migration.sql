5dbafef1aa7250ae4c52212e1ea187aa4a3eae2a
Enzo Bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-02-21 15:41:37 +0100

    Update migration prisma

--- prisma/migrations/20230221141807_update_database/migration.sql
+++
@@ -1,5 +0,0 @@
--- RenameForeignKey
-ALTER TABLE "bookmark" RENAME CONSTRAINT "bookmark_tool_id_fkey" TO "favorite_tool_id_fkey";
-
--- RenameForeignKey
-ALTER TABLE "bookmark" RENAME CONSTRAINT "bookmark_user_id_fkey" TO "favorite_user_id_fkey";
