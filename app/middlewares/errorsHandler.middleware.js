fd5a004fa218bfcf661f1b69e47d3af74ce3c7c6
Enzo Bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-02-21 17:03:06 +0100

    Better zod error handling and eslint config update

--- app/middlewares/errorsHandler.middleware.js
+++ app/middlewares/errorsHandler.middleware.js
@@ -3,7 +3,7 @@ import { ZodError } from "zod";
 async function errorsHandler(error, request, response, next) {
   // Handle Zod Errors
   if (error instanceof ZodError)
-    return response.status(403).json(...error.issues);
+    return response.status(403).json(error.flatten().fieldErrors);
 
   // Handle General Errors
   response.status(500).json(error);
