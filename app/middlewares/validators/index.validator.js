ab3ef5ec743c9ab41ab95719ebe28d193646c5f3
Enzo Bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-02-21 15:46:10 +0100

    Update strcuture vaidator

--- app/middlewares/validators/index.validator.js
+++ app/middlewares/validators/index.validator.js
@@ -1,5 +1,5 @@
 import validateTool from "./tool.validator.js";
 import validateUser from "./user.validator.js";
-import userValidator from "./user.validator.js";
+import validateBookmark from "./bookmark.validator.js";
 
-export { validateUser, validateTool, userValidator };
+export { validateUser, validateTool, validateBookmark };
