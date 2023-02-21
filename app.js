a381382964e1df0c91f28cb316f3bbb20ee7b561
Sousse1982 <kraiem.r@gmail.com>
2023-02-21 15:17:01 +0100

    update + jsdoc

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 import express from "express";
 // import logger from "morgan";
 // import fs from "fs";
- import cors from "cors";
+import cors from "cors";
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 import router from "./app/routes/index.router.js";
 // import { handleErrors } from "./app/middlewares/errorHandler.js";
 
 const app = express();
+
+import expressJSDocSwagger from "express-jsdoc-swagger";
+
+const options = {
+    info: {
+        version: '1.0.0',
+        title: 'devzone',
+        license: {
+            name: 'MIT',
+        },
+    },
+    // Chemin de la doc
+    swaggerUIPath: '/devzone-api',
+    security: {
+        BasicAuth: {
+            type: 'http',
+            scheme: 'basic',
+        },
+    },
+    baseDir: __dirname,
+    // Glob pattern to find your jsdoc files (multiple patterns can be added in an array)
+    filesPattern: './**/*.js',
+};
+
+expressJSDocSwagger(app)(options);
 
  app.use(cors());
 // app.use(logger("dev"));
@@ -17,6 +46,7 @@ app.use(express.static("public"));
 
 app.use(router);
 
+//app.use(errorService.manage);
 //app.use(handleErrors);
 
 export default app;
