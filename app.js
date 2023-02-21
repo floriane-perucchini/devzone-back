5c3680d6c2471e86f1c1374306d23c8d70e1905f
Enzo Bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-02-21 17:35:42 +0100

    Add log handling with morgan

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
-// import logger from "morgan";
-// import fs from "fs";
+import logger from "morgan";
+import fs from "fs";
 import cors from "cors";
 
 import router from "./app/routes/index.router.js";
@@ -9,8 +9,12 @@ import { errorsHandler } from "./app/middlewares/index.middleware.js";
 const app = express();
 
 app.use(cors());
-// app.use(logger("dev"));
-// app.use(logger("common", { stream: fs.createWriteStream("./access.log", { flags: "a" }) }));
+app.use(logger("dev"));
+app.use(
+  logger("common", {
+    stream: fs.createWriteStream("./logs/access.log", { flags: "a" }),
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
