90ccead771a2451238897722ef811dda18eeb2cf
Enzo Bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-02-23 16:31:52 +0100

    Update controllers with new database schema

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -1,4 +1,5 @@
 import db from "../models/index.datamapper.js";
+import bcrypt from "bcrypt";
 
 const mainController = {
   login: async function (request, response, next) {
@@ -7,6 +8,12 @@ const mainController = {
       const checkUser = await db.main.checkUser({ username, email, password });
       if (!checkUser)
         throw new Error("Your email/username or password is not correct.");
+
+      const user = db.main.getUser({ username, email });
+      const checkPassword = await bcrypt.compare(password, user.password);
+      if (!checkPassword)
+        throw new Error("Your email/username or password is not correct.");
+
       response.json("OK");
     } catch (error) {
       error.status = 403;
@@ -20,8 +27,14 @@ const mainController = {
       if (checkUser.username)
         throw new Error("This username is already in use.");
 
-      await db.user.create(request.body);
-      response.status(201).json("User has been created successfully.");
+      // Hash password
+      const newUser = request.body;
+      const { password } = newUser;
+      newUser.password = await bcrypt.hash(password, 10);
+      newUser.confirmedPassword.delete;
+
+      await db.user.create(newUser);
+      response.status(201).json("Registered successfully.");
     } catch (error) {
       next(error);
     }
