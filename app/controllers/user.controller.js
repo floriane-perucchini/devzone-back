dc4008fdce17b07931b4ede339d0f0ee8d337100
Enzo Bacqueyrisses <enzo.bacqueyrisses@gmail.com>
2023-02-24 13:15:28 +0100

    Update branch

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -14,23 +14,9 @@ const userController = {
     const { id } = request.params;
 
     try {
-<<<<<<< Updated upstream
-      const user = await prisma.user.findUnique({
-        where: { id: Number(id) },
-        include: {
-          tool: true,
-        },
-      });
-
-      response.json({ user });
-
-      const users = await user.get(id);
-      response.json({ users });
-=======
       const user = await db.user.get(id);
 
       response.json(user);
->>>>>>> Stashed changes
     } catch (error) {
       next(error);
     }
@@ -46,42 +32,6 @@ const userController = {
     if (email) user.email = email.toLowerCase();
     if (password) user.password = password;
     if (username) user.password = username.toLowerCase();
-
-<<<<<<< Updated upstream
-    try {
-      const user = await prisma.user.update({
-        where: { id: Number(id) },
-        data: {
-          email: String(email),
-          password: String(password),
-          username: String(username),
-          tool_id: String(tool_id),
-        },
-      });
-      const user = await db.user.create(user);
-
-      response.json({ user });
-    } catch (error) {
-      next(error);
-    }
-=======
-    // try {
-    //   const user = await prisma.user.update({
-    //     where: { id: Number(id) },
-    //     data: {
-    //       email: String(email),
-    //       password: String(password),
-    //       username: String(username),
-    //       tool_id: String(tool_id),
-    //     },
-    //   });
-    //   const user = await db.user.create(user);
-    //
-    //   response.json({ user });
-    // } catch (error) {
-    //   next(error);
-    // }
->>>>>>> Stashed changes
   },
 
   delete: async function (request, response, next) {
