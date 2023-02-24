b8b7d4cc27d5d3028db5863efa72aaa6a35d0f34
Sousse1982 <kraiem.r@gmail.com>
2023-02-24 13:28:09 +0100

    db

--- data/seeding.sql
+++ data/seeding.sql
@@ -1,20 +1,21 @@
 BEGIN;
 
-INSERT INTO public."tool"(
-	 name, logo, description)
-	VALUES ("Github", "test", "git"),
-	VALUES ("Stackoverflow", "test2", "stack"),
-	VALUES ("MDN", "test3", "Mozilla");
-
-
-INSERT INTO public."user"(
-	 email, password, firstname, lastname, username, avatar,)
+INSERT INTO public."Tool"("name", "logo", "description")
+	 
 	VALUES 
-  ("test@test.fr","ddsqdlsj" , "test1","testbis", "toto", "test"),
-  (test@test.fr, "dsqhdk", "test2", "test2", "test", "test"),
-  (test@test.fr, "dsdffqqsd", "test3", "test3", "test", "test"),
-  (test@test.fr, "dsdffqqsd", "test3", "test3", "test", "test"),
-  (test@test.fr, "dsdffqqsd", "test3", "test3", "test", "test");
+	 ("Github", "test", "git"),
+	 ("Stackoverflow", "test2", "stack"),
+	 ("MDN", "test3", "Mozilla");
+
+
+INSERT INTO public."User"("email", "password", "firstname", "lastname", "username", "avatar")
+	 
+	VALUES 
+  ("test@test.fr", "ddsqdlsj", "test1", "testbis", "toto", "test1"),
+  ("test@test.fr", "dsqhdk", "test2", "test2", "test", "test10"),
+  ("test@test.fr", "dsdffqqsd", "test3", "test3", "test", "test15"),
+  ("test@test.fr", "dsdffqqsd", "test3", "test3", "test", "test5"),
+  ("test@test.fr", "dsdffqqsd", "test3", "test3", "test", "test8");
 
 
 
